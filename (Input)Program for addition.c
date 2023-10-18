cf6ecb544ba619968c545ca91df8266b03ffb836
MAHISHARMA-2023A1R078 <146845167+MAHISHARMA-2023A1R078@users.noreply.github.com>
2023-10-18 18:02:39 +0530

    Add files via upload

---
+++ (Input)Program for addition.c
@@ -0,0 +1,17 @@
+//title of program
+//program for addition 
+#include<stdio.h>
+//function heading
+void main()
+{
+//variable declaration
+int a,b,c;
+//output statement
+printf("enter two numbers a,b");
+//input statement 
+scanf("%d%d",&a,&b);
+//formula for addition
+c=a+b;
+//output statement
+printf("sum=%d\n",c);
+}
