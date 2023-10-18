0cdcf4fbfb68b21da5a51bbb86f6def2c06759b7
MAHISHARMA-2023A1R078 <146845167+MAHISHARMA-2023A1R078@users.noreply.github.com>
2023-10-18 20:05:24 +0530

    Add files via upload

---
+++ (Input)Program of Explicit.c
@@ -0,0 +1,14 @@
+//title of program
+//Program of Explicit
+#include<stdio.h>
+//function heading
+void main()
+{
+//variable declaration
+int a=7,b=2;
+//input statement to find
+float c;
+c=(float)a/(float)b;
+//outout statement 
+printf("Ans=%f",c);
+}
