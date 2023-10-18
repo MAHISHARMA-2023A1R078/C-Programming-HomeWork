4936298e3717db4f34bd14ae04dcf0b79eb586b1
MAHISHARMA-2023A1R078 <146845167+MAHISHARMA-2023A1R078@users.noreply.github.com>
2023-10-18 17:47:05 +0530

    Add files via upload

---
+++ (Input)Program to print the value of char and number.c
@@ -0,0 +1,23 @@
+//title of program 
+//program to print the value of char and number
+#include<stdio.h>
+//function heading
+void main()
+{
+//variable declaration
+int n;
+//output statement 
+printf("enter the number");
+//input statement 
+scanf("%d",&n);
+//character declaration
+char c;
+//output statement 
+printf("enter the char");
+//input statement 
+scanf("%c%c",&c,&c);
+//output statement 
+printf("the number is=%d\n",n);
+//output statement 
+printf("the char is=%c",c);
+}
