22d1baf7da606eb356208e2e753e623aafe46c5b
MAHISHARMA-2023A1R078 <146845167+MAHISHARMA-2023A1R078@users.noreply.github.com>
2023-10-18 19:54:18 +0530

    Add files via upload

---
+++ (Input)Program of Implicit.c
@@ -0,0 +1,13 @@
+//title of program
+//Program of Implicit
+#include<stdio.h>
+//function heading
+void main()
+{
+//variable declaration
+int a=7,b=2;
+//input statement to find
+float c;
+c=a/b;
+printf("Ans=%f",c);
+}
