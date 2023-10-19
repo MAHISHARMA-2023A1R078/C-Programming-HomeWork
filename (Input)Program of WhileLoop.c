0361a1b500cd45b8e2eae3154e2cd64b6548ce38
MAHISHARMA-2023A1R078 <146845167+MAHISHARMA-2023A1R078@users.noreply.github.com>
2023-10-19 20:05:38 +0530

    Add files via upload

---
+++ (Input)Program of WhileLoop.c
@@ -0,0 +1,16 @@
+//title of program
+//Program of WhileLoop
+#include<stdio.h>
+//function heading
+void main()
+{
+//variable declaration
+int i=0;
+//condition of while loop
+while(i<=10)
+{
+//Output statement 
+printf("%d\n",i);
+++i;
+}
+}
