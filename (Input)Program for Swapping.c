be2b6fecefb4c0d762772b7ef720dd985a790960
MAHISHARMA-2023A1R078 <146845167+MAHISHARMA-2023A1R078@users.noreply.github.com>
2023-10-19 16:51:42 +0530

    Add files via upload

---
+++ (Input)Program for Swapping.c
@@ -0,0 +1,15 @@
+//title of program
+//Program for Swapping 
+#include<stdio.h>
+//function heading
+void main()
+{
+//variable declaration
+int a;
+//output statement
+printf("enter the number 1 or 2");
+//input statement
+scanf("%d",&a);
+//output statement 
+printf("%d\n",3-a);
+}
