b3a59304a64eff6ec92382cf8bccee55231cc5ca
MAHISHARMA-2023A1R078 <146845167+MAHISHARMA-2023A1R078@users.noreply.github.com>
2023-10-19 19:57:11 +0530

    Add files via upload

---
+++ (Input)Program of ForLoop.c
@@ -0,0 +1,15 @@
+//title of program
+//Program of ForLoop
+#include<stdio.h>
+//Function Heading
+void main()
+{
+//variable declaration
+int i;
+//condition of ForLoop
+for(i=1;i<=10;i++)
+{
+//output statement 
+printf("Aakash\n");
+}
+}
