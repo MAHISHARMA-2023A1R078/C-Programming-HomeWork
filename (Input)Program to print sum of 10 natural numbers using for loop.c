1d7902dd19e6c2a83844ac14d6aa50128bd2a824
MAHISHARMA-2023A1R078 <146845167+MAHISHARMA-2023A1R078@users.noreply.github.com>
2023-10-19 21:15:26 +0530

    Add files via upload

---
+++ (Input)Program to print sum of 10 natural numbers using for loop.c
@@ -0,0 +1,17 @@
+//title of program
+//Program to print sum of 10 natural numbers using for loop
+#include<stdio.h>
+//function heading
+void main()
+{
+//variable declaration
+int i,sum;
+//condition of For Loop
+for(i=1;i<=10;i++)
+{
+sum=sum+i;
+//output statement 
+printf("sum=sum+i\n");
+printf("%d\n",sum);
+}
+}
