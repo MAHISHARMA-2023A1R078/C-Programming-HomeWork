38f32e8dca6a67f860e0e3496d744c49ac2d8873
MAHISHARMA-2023A1R078 <146845167+MAHISHARMA-2023A1R078@users.noreply.github.com>
2023-10-30 20:36:01 +0530

    Add files via upload

---
+++ (Input)Program to find the ASCII values of A,B,a,b.c
@@ -0,0 +1,15 @@
+//title of program
+//Program to find the ASCII values of A,B,a,b;
+#include<stdio.h>
+//function heading
+void main()
+{
+//character declaration
+char c;
+//output statement to enter the values
+printf("enter a char");
+//input statement 
+scanf("%c",&c);
+//output statement(ASCII value)
+printf("ASCII value of c=%d",c);
+}
