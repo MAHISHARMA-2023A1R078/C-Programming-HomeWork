84319ee64f5bf269c698e8788462796067450b21
MAHISHARMA-2023A1R078 <146845167+MAHISHARMA-2023A1R078@users.noreply.github.com>
2023-10-31 18:10:08 +0530

    Add files via upload

---
+++ (Input)Program to print the numbers in triangle form.c
@@ -0,0 +1,20 @@
+//titile of program
+//Program to print the numbers in triangle form
+#include<stdio.h>
+//header file
+void main()
+{
+//variable declaration
+int i,j;
+//condition to print the umbers in triangle form
+for(i=0;i<=5;i++)
+{
+for(j=0;j<=i;j++)
+{
+//output statement 
+printf("%d",i);
+}
+//output statement 
+printf("\n");
+}
+}
