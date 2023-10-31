4754ff05e87d2ae15a9e2f5d6091f506dafb74b7
MAHISHARMA-2023A1R078 <146845167+MAHISHARMA-2023A1R078@users.noreply.github.com>
2023-10-31 18:06:10 +0530

    Add files via upload

---
+++ (Input)Program to print the numbers in rectangle form.c
@@ -0,0 +1,20 @@
+//titile of program
+//Program to print the numbers in rectangle form
+#include<stdio.h>
+//header file
+void main()
+{
+//variable declaration
+int i,j;
+//condition to print the umbers in rectangle form
+for(i=0;i<=5;i++)
+{
+for(j=0;j<=5;j++)
+{
+//output statement 
+printf("%d",j);
+}
+//output statement 
+printf("\n");
+}
+}
