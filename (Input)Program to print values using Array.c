a7070480eb08e6ac2d44cfb6839e301ebe403022
MAHISHARMA-2023A1R078 <146845167+MAHISHARMA-2023A1R078@users.noreply.github.com>
2023-11-02 21:23:11 +0530

    Add files via upload

---
+++ (Input)Program to print values using Array.c
@@ -0,0 +1,15 @@
+//title of program
+//Program to print values using Array
+#include<stdio.h>
+//function heading
+void main()
+{
+//variable declaration(Array)
+int a[60];
+a[0]=10;
+a[1]=20;
+a[2]=30;
+a[3]=40;
+//output statement 
+printf("%d\n%d\n%d\n%d\n",a[0],a[1],a[2],a[3]);
+}
