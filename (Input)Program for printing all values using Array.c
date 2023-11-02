68478a5d0802df485fabc9e27e51df11b2711e76
MAHISHARMA-2023A1R078 <146845167+MAHISHARMA-2023A1R078@users.noreply.github.com>
2023-11-02 21:44:46 +0530

    Add files via upload

---
+++ (Input)Program for printing all values using Array.c
@@ -0,0 +1,15 @@
+//title of program
+//Program for printing all values using Array
+#include<stdio.h>
+//function heading
+void main()
+{
+//variable declaration
+int a[]={1,2,3,4,5,6,7,8,9,10},i;
+//condition of for loop (used in array)
+for(i=0;i<10;i++)
+{
+//output statement 
+printf("%d\n",a[i]);
+}
+}
