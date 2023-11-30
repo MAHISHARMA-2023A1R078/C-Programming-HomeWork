64e4b722421770ac90947728c174eed5dfad37e0
MAHISHARMA-2023A1R078 <146845167+MAHISHARMA-2023A1R078@users.noreply.github.com>
2023-11-30 19:34:07 +0530

    Add files via upload

---
+++ (Input)Program based on function without argument with return.c
@@ -0,0 +1,23 @@
+//title of program
+//Program based on function without argument with return
+#include<stdio.h>
+//function heading
+void main()
+{
+//variable declaration
+int add();
+int c;
+c=add();
+//output statement 
+printf("sum=%d",c);
+}
+int add()
+{
+int x,y;
+//output statement 
+printf("enter two numbers");
+//input statement 
+scanf("%d%d",&x,&y);
+int z=x+y;
+return z;
+}
