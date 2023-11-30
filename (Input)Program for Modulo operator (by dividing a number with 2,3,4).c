dc580566a8c77356c045cb9b73cc1b3e063c7f3d
MAHISHARMA-2023A1R078 <146845167+MAHISHARMA-2023A1R078@users.noreply.github.com>
2023-11-30 18:31:08 +0530

    Add files via upload

---
+++ (Input)Program for Modulo operator (by dividing a number with 2,3,4).c
@@ -0,0 +1,22 @@
+//title of program
+//Program for Modulo operator (by dividing a number with 2,3,4)
+#include<stdio.h>
+//function heading
+void main()
+{
+//variable declaration
+int a=2,b=3,c=4,d,num;
+//output statement 
+printf("enter the num");
+//input statement 
+scanf("%d",&num);
+d=num%a;
+//output statement 
+printf("%d\n",d);
+d=num%b;
+//output statement 
+printf("%d\n",d);
+d=num%c;
+//output statement 
+printf("%d\n",d);
+}
